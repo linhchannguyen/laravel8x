b87f317105a46c4e36eb6638da48a0cc3d4a6682
channlominext <106024853+channlominext@users.noreply.github.com>
2022-07-04 17:16:14 +0700

    auth route

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -2,23 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
 use Validator;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class JWTController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct() {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    }
-
     /**
      * Get a JWT via given credentials.
      *
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\JWTController;
 |
 */
 
-Route::group(['middleware' => 'api'], function($router) {
+Route::post('/login', [JWTController::class, 'login']);
+Route::group(['middleware' => 'jwt.verify'], function() {
     Route::post('/register', [JWTController::class, 'register']);
-    Route::post('/login', [JWTController::class, 'login']);
     Route::post('/logout', [JWTController::class, 'logout']);
     Route::post('/refresh', [JWTController::class, 'refresh']);
-    Route::post('/profile', [JWTController::class, 'profile']);
+    Route::post('/profile', [JWTController::class, 'userProfile']);
 });
